2feb12243eb61d32fdaa228b7f698fc2c35e08fb
Jainam Shah <meetjainam39@gmail.com>
2019-03-12 16:05:08 +0530

    Update stock pred.py

--- stock pred.py
+++ stock pred.py
@@ -1,89 +1,56 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats.stats import pearsonr
 import numpy as np
-
-data = pd.read_csv('RELIANCE.NS.csv')
-
-# print(data.head())
-# print(data.max())
-print(data.info())
-data.set_index('Date', inplace=True)
-
-# print(data.head())
-
-plt.figure(figsize=(16, 10))
-plt.plot(data['Close'], label='Close Price History')
-plt.legend()
-# plt.show()
-
-data['Date'] = pd.to_datetime(data.index, format='%Y/%m/%d')
-
-data1 = data.sort_index(ascending=True, axis=0)
-
-df = pd.DataFrame(index=range(0, len(data)), columns=['Date', 'Close'])
-
-
-for i in range(0, len(data1)):
-     df['Date'][i] = data['Date'][i]
-     df['Close'][i] = data['Close'][i]
-
-df.set_index('Date', inplace=True)
-df.dropna(inplace=True)
-print(df.head(20))
-
-train = df[:987]
-valid = df[987:]
-
-# from sklearn.model_selection import train_test_split
-# xtrain, ytrain, xtest, ytest = train_test_split(data['Date'], data['Close'], test_size=0.25)
-
-x_train = np.array(train.index).reshape(-1, 1)
-y_train = np.array(train['Close']).reshape(-1, 1)
-
-x_valid = np.array(valid.index).reshape(-1, 1)
-y_valid = np.array(valid['Close']).reshape(-1, 1)
-
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-model = LinearRegression()
-model.fit(x_train, y_train)
+from sklearn.model_selection import train_test_split
+import datetime
 
-preds = model.predict(x_valid)
-rms = np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)), 2)))
+
+df = pd.read_csv('RELIANCE.NS.csv')
+df.dropna(inplace=True)
+
+#Get Data From Quandl
+df = df.reset_index()
+prices = df['Close'].tolist()
+dates = df.index.tolist()
+
+# prices = df['Close']
+# dates = df['Date']
+# df.set_index(dates, inplace=True)
+
+#Convert to 1d Vector
+dates = np.reshape(dates, (len(dates), 1))
+prices = np.reshape(prices, (len(prices), 1))
+
+# data = df.sort_index(ascending=True, axis=0)
+# new_data = pd.DataFrame(index=range(0, len(df)), columns=['Date', 'Close'])
+#
+# for i in range(0, len(data)):
+#     new_data['Date'][i] = data['Date'][i]
+#     new_data['Close'][i] = data['Close'][i]
+#
+# new_data['Date'] = pd.to_datetime(new_data['Date'], format='%Y/%m/%d')
+#
+# new_data.set_index(new_data['Date'], inplace=True)
+# new_data.dropna(inplace=True)
+#
+# print(new_data.head())
+
+regressor = LinearRegression()
+
+# #Splitting the dataset into the Training set and Test set
+xtrain, xtest, ytrain, ytest = train_test_split(dates, prices, test_size=0.25, random_state=42)
+
+regressor.fit(xtrain, ytrain)
+
+# pred = regressor.predict(xtrain)
+preds = regressor.predict(xtest)
+
+
+rms = np.sqrt(np.mean(np.power((np.array(ytest)-np.array(preds)), 2)))
 print(rms)
-
-# valid['Predictions'] = 0
-# valid['Predictions'] = preds
-#
-# valid.index = df[987:].index
-# train.index = df[:987].index
-#
-# plt.plot(train['Close'])
-# plt.plot(valid[['Close', 'Predictions']])
-# plt.show()
-
-# # dates = np.reshape(Dates, (len(Dates), 1))
-# # prices = np.reshape(Prices, (len(Prices), 1))
-#
-# df['Date'] = pd.to_datetime(df.Date, format='%Y/%m/%d')
-#
-#
-#
-# print(df.head(15))
-#
-#
-#
-# x_train = np.array(train['Date']).reshape(-1, 1)
-# y_train = np.array(train['Close'])
-#
-# x_test = np.array(test['Date']).reshape(-1, 1)
-# y_test = np.array(test['Close'])
-#
-# from sklearn import linear_model
-#
-# model = linear_model.LinearRegression()
-# model.fit(x_train, y_train)
-#
-# pred = model.predict(x_test)
-# # rms = np.sqrt(np.mean(np.power((np.array(y_test)-np.array(pred)), 2)))
-# # print(rms)
+print('Accuracy of the Linear Regression Model', regressor.score(xtest, ytest))
+plt.scatter(xtrain, ytrain, color = 'red')
+plt.plot(xtrain, regressor.predict(xtrain), color = 'blue')
+plt.title('Actual Price VS Predicted Price')
+plt.show()
